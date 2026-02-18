8c8fe06c7dd8332bc1afbf5a131e2b3f27ffc94b
folarinstephen21 <249096587+folarinstephen21@users.noreply.github.com>
2026-02-18 13:13:48 +0100

    fixed dynamic svg rendering issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 const weatherInfo = document.getElementById("weatherInfo")
 const weatherUi = document.getElementById("contentBox")
 
-console.log("welcome")
+console.log("Hello")
 const weatherBtn = document.getElementById("action")
 const userInput = document.getElementById("location")
 
@@ -17,9 +17,14 @@ weatherBtn.addEventListener("click", async () => {
     const searchString = `${linkIndex[0]}${input}?key=MFBH9JFDBRJHVS69GH4JGKLU5`;
     console.log(searchString)
     userInput.value = "";
-    const result =await callApi(searchString)
-    console.log(result)
-    renderWeather(result, weatherUi)
+    const result =await callApi(searchString);
+    // console.log(result)
+    const rawIcon = result.info.icon;
+    const normalized = normalizeCondition(rawIcon);
+
+    const weatherIcon = await loadWeatherIcon(normalized);
+
+    renderWeather(result, weatherUi, weatherIcon)
 })
 
 async function callApi (searchString){
@@ -34,20 +39,43 @@ async function callApi (searchString){
 }
 
 async function loadWeatherIcon(condition) {
+  const iconMap = {
+    rain: "rainy",
+    cloudy: "cloudy",
+    clear: "sun-line",
+  };
+
+  const iconName = iconMap[condition?.toLowerCase()];
+
+  if (!iconName) {
+    console.warn("No matching icon for:", condition);
+    return null;
+  }
+
   try {
-    const icon = await import(`./icons/${condition}.svg`);
+    const icon = await import(`./icons/${iconName}.svg`);
     return icon.default;
   } catch (err) {
     console.error("Icon not found:", err);
+    return null;
   }
 }
 
-async function renderWeather(result, weatherPanel) {
-    weatherPanel.innerHTML = ""
-//   const img = document.createElement("img");
 
-//   const iconSrc = await loadWeatherIcon(condition);
-//   img.src = iconSrc;
+
+function normalizeCondition(iconString) {
+  if (!iconString) return null;
+
+  if (iconString.includes("rain")) return "rain";
+  if (iconString.includes("cloud")) return "cloudy";
+  if (iconString.includes("clear")) return "clear";
+
+  return null;
+}
+
+async function renderWeather(result, weatherPanel, weatherIcon) {
+    weatherPanel.innerHTML = ""
+  const img = document.createElement("img");
 
 //   document.querySelector("#weather").appendChild(img);
 const heading = document.createElement("h3");
@@ -58,6 +86,10 @@ para1.innerText = result.info.description;
 
 weatherPanel.appendChild(heading);
 weatherPanel.appendChild(para1)
+if (weatherIcon) {
+  img.src = weatherIcon;
+  weatherPanel.appendChild(img);
+}
 }
 
 
